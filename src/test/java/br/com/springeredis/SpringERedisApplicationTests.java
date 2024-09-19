a95f3702e2d71ee68ed70b0a1b76f9e043aa3ec9
cleitonsilva14 <cleiton18ads@gmail.com>
2024-09-19 14:49:32 -0300

    Subindo o tutorial

---
+++ src/test/java/br/com/springeredis/SpringERedisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springeredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringERedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
